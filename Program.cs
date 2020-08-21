524eef95a9acefef922b329d0a9c82ed7a588549
tobias.hartwich <tobias.hartwich@hotmail.de>
2020-08-21 12:15:05 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Laufwerkermittlung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            System.IO.DriveInfo[] AlleLaufwerke = System.IO.DriveInfo.GetDrives();
+            for (int i = 0; i <= AlleLaufwerke.Length-1; i++)
+            {
+                string LWBuchstabe;
+                string LWType;
+                string LWFormat;
+                string LWFrei;
+                string LWGroesse;
+
+                try
+                {
+                    LWBuchstabe = AlleLaufwerke[i].ToString();
+                }
+                catch
+                {
+                    LWBuchstabe = "unbekannt";
+                }
+                try
+                {
+                    LWType = AlleLaufwerke[i].DriveType.ToString();
+                }
+                catch
+                {
+                    LWType = "unbekannt";
+                }
+                try
+                {
+                    LWFormat = AlleLaufwerke[i].DriveFormat.ToString();
+                }
+                catch
+                {
+                    LWFormat = "unbekannt";
+                }
+                try
+                {
+                    LWFrei = AlleLaufwerke[i].TotalFreeSpace.ToString();
+                }
+                catch
+                {
+                    LWFrei = "unbekannt";
+                }
+                try
+                {
+                    LWGroesse = AlleLaufwerke[i].TotalSize.ToString();
+                }
+                catch
+                {
+                    LWGroesse = "unbekannt";
+                }
+
+                Console.WriteLine("Label: {0} Typ: {1} Format: {2} Frei: {3} Byte Total: {4} Byte", LWBuchstabe, LWType, LWFormat, LWFrei, LWGroesse);
+            }
+        }
+    }
+}
